87336765a037c0fccf11163415edf8abc2ac9ab8
cesarsnina <cesar.nina05@gmail.com>
2021-11-30 21:35:50 +0000

    updated the json file for the restaurant and seed file

--- README.md
+++
@@ -1,1 +0,0 @@
-# Multiverse-Activites-2
--- week4/Card-Collecting-database/db.js
+++
@@ -1,9 +0,0 @@
-const {Sequelize, DataTypes, Model} = require('sequelize')
-
-const db = new Sequelize('database', 'username', 'password', {
-    dialect: 'sqlite',
-    storage: './Card.sqlite',
-    logging: false
-})
-
-module.exports = {db, DataTypes, Model}
